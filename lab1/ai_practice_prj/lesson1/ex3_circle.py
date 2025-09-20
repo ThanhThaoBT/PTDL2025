22bcfc35e879a45ec6a75fc3c3032d66796e32f4
ThanhThaoBT <126171797+ThanhThaoBT@users.noreply.github.com>
2025-09-20 09:42:34 +0700

    Add files via upload

---
+++ lab1/ai_practice_prj/lesson1/ex3_circle.py
@@ -0,0 +1,11 @@
+from math import pi
+
+print("Tinh chu vi va dien tich hinh tron")
+r = float(input("Nhap ban kinh: "))
+C = 2 * pi * r
+S = pi * r**2
+
+print(f"Chu vi hinh tron la: {C}")
+print(f"Dien tich hinh tron la: {S}")
+
+
